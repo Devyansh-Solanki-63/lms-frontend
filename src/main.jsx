d8d62e82a730cc5cd7f9c61de47675f6b745ccfb
Devyansh Solanki <panchaldevyansh632003@gmail.com>
2024-07-28 23:16:38 +0530

    added react-hot-toast and some eslint changes for importing files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import App from './App.jsx'
-import ReactDOM from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
+
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>
 )
