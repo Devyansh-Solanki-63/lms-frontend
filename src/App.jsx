28ef6605c851f265efbc0059f9ce87a318ad620c
Devyansh Solanki <panchaldevyansh632003@gmail.com>
2024-07-30 08:49:33 +0530

    Added footer component and added postcss.config.js file to use tailwindcss classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 
+import {Routes} from 'react-router-dom'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <>
-      hello
+      <Footer />
     </>
   )
 }
