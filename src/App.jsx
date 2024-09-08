e55c8f25495f86505e3660b5527ec830faa71b6e
Devyansh Solanki <panchaldevyansh632003@gmail.com>
2024-09-08 23:22:45 +0530

    not-found page added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {Route, Routes} from 'react-router-dom'
 // import Footer from './components/Footer'
 import HomePage from './Pages/HomePage'
 import AboutUs from './Pages/AboutUs'
+import NotFound from './Pages/NotFound'
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
+
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
   )
