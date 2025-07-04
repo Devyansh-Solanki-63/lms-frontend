ed242f243139de5fd96c0014cb42be1c3327a8bb
Devyansh-Lucent <devyansh.solanki@lucentinnovation.com>
2025-07-04 19:43:44 +0530

    checkout page enhanced

--- src/Pages/Payment/Checkout.jsx
+++ src/Pages/Payment/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { toast } from 'react-hot-toast'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
@@ -13,16 +13,18 @@ const Checkout = () => {
     const razorpayKey = useSelector(state => state.razorpay.key)
     const subscription_id = useSelector(state => state.razorpay.subscription_id)
     const isPaymentVerified = useSelector(state => state.razorpay.isPaymentVerified)
-    const userData = useSelector(state => state?.user?.data)
+    const userData = useSelector(state => state?.auth?.data)
     const paymentDetails = {
         razorpay_payment_id: "",
         razorpay_subscription_id: "",
         razorpay_signature: ""
     }
 
+    const [isLoading, setIsLoading] = useState(false)
+
     const handleSubscription = async (e) => {
         e.preventDefault()
-        if(!razorpayKey || !subscription_id){
+        if (!razorpayKey || !subscription_id) {
             toast.error("Something went wrong..!")
             return
         }
@@ -31,8 +33,12 @@ const Checkout = () => {
             subscription_id: subscription_id,
             name: "CourseCracker Pvt. Ltd.",
             description: "Subscription",
-            theme: {color: '#F37254'},
-            handler: async function (response){
+            theme: { color: '#F37254' },
+            prefill: {
+                email: userData.email,
+                name: userData.name
+            },
+            handler: async function (response) {
                 paymentDetails.razorpay_payment_id = response.razorpay_payment_id;
                 paymentDetails.razorpay_subscription_id = response.razorpay_subscription_id;
                 paymentDetails.razorpay_signature = response.razorpay_signature;
@@ -48,47 +54,53 @@ const Checkout = () => {
     }
 
     const load = async () => {
+        setIsLoading(true)
         await dispatch(getRazorpayId())
         await dispatch(purchaseCourseBundle())
+        setIsLoading(false)
     }
 
     useEffect(() => {
-      load()
+        load()
     }, [])
-    
+
 
     return (
         <HomeLayout>
-            <form
-                onSubmit={handleSubscription}
-                className="min-h-[90vh] flex items-center justify-center text-white"
-            >
-                <div className="w-80 h-[26rem] flex flex-col justify-center shadow-[0_0_10px_black] rounded-lg relative">
-                    <h1 className="bg-yellow-500 absolute top-0 w-full text-center py-4 text-2xl font-bold rounded-tl0lg rounded-tr-lg">Subscription Bundle</h1>
-                    <div className="px-4 space-y-5 text-center">
-                        <p className="text-[17px]">
-                            This purchase will allow you to access all available course
-                            of our platform for {" "} 
-                            <span className="text-yellow-500 font-bold">
-                                <br />
-                                1 Year duration
-                            </span> { " " }
-                            All the existing and new launched courses will be also available
-                        </p>
+            {!isLoading ? (
+                <form
+                    onSubmit={handleSubscription}
+                    className="min-h-[90vh] flex items-center justify-center text-white"
+                >
+                    <div className="w-80 h-[26rem] flex flex-col justify-center shadow-[0_0_10px_black] rounded-lg relative">
+                        <h1 className="bg-yellow-500 absolute top-0 w-full text-center py-4 text-2xl font-bold rounded-tl0lg rounded-tr-lg">Subscription Bundle</h1>
+                        <div className="px-4 space-y-5 text-center">
+                            <p className="text-[17px]">
+                                This purchase will allow you to access all available course
+                                of our platform for {" "}
+                                <span className="text-yellow-500 font-bold">
+                                    <br />
+                                    1 Year duration
+                                </span> {" "}
+                                All the existing and new launched courses will be also available
+                            </p>
 
-                        <p className="flex items-center justify-center gap-1 text-2xl font-bold text-yellow-500">
-                            <BiRupee /><span>499</span> only
-                        </p>
-                        <div className="text-gray-200">
-                            <p>100% refund on cancellation</p>
-                            <p>* Terms and conditions applied *</p>
+                            <p className="flex items-center justify-center gap-1 text-2xl font-bold text-yellow-500">
+                                <BiRupee /><span>499</span> only
+                            </p>
+                            <div className="text-gray-200">
+                                <p>100% refund on cancellation</p>
+                                <p>* Terms and conditions applied *</p>
+                            </div>
+                            <button type="submit" className="bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 absolute bottom-0 w-full left-0 text-xl font-bold rounded-bl-lg rounded-br-lg py-2">
+                                Buy now
+                            </button>
                         </div>
-                        <button type="submit" className="bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 absolute bottom-0 w-full left-0 text-xl font-bold rounded-bl-lg rounded-br-lg py-2">
-                            Buy now
-                        </button>
                     </div>
-                </div>
-            </form>
+                </form>
+            ) : (
+                <div className='min-h-[90vh] flex items-center justify-center text-white'>loading...</div>
+            )}
         </HomeLayout>
     )
 }
